c011a47943aa23dfd21dd342d559fed3a7de52f6
Lis <lp598@kent.ac.uk>
2026-03-10 20:18:32 +0000

    added contact details and time stamp

--- models.py
+++ models.py
@@ -13,4 +13,17 @@ class Person:
     
     def get_phone(self):
         return self.phone
+    
+    def get_email(self):
+        return self._email
+    
+    def get_created_at(self):
+        return self._created_at
+    
+
+class Contact(Person):
+    def __init__(self, name, phone, email):
+        super().__init__(name,phone)
+        self._email = email
+        self._created_at = datetime.now().strftime("%Y-%M-%D %H:%M:%S")
 
