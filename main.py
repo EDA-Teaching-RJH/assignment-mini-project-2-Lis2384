8e6b9f0373a9344dc45a8608942feec2a32b4505
Lis <lp598@kent.ac.uk>
2026-03-10 22:28:03 +0000

    ran test, code ok

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from models import Contact
 
-contact1 = Contact("Lis Pireva", "1234567890", "lis@example.com")
-
-print(contact1)
+try:
+    contact1 = Contact("Lis Pireva", "1234567890", "lis@example.com")
+    print(contact1)
+except ValueError as error:
+    print(error)
--- test_contacts.py
+++ test_contacts.py
@@ -3,8 +3,16 @@ from models import Contact
 
 class TestContact(unittest.TestCase):
     def test_contact_creation(self):
-        contact = Contact("Lis Pireva", "1234567890", "lis@example.com")
+        contact = Contact("Lis Pireva", "+447718833017", "lis@example.com")
         self.assertEqual(contact.get_email(), "lis@example.com")
+
+    def test_invalid_email(self):
+        with self.assertRaises(ValueError):
+            Contact("Lis Pireva", "+447718833017", "fsdfkjsvb")
+
+    def test_invalid_phone(self):
+        with self.assertRaises(ValueError):
+                Contact("Lis pireva", "435", "lis@example.com")
 
 if __name__ == "__main__":
     unittest.main()
