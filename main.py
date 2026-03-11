acd3b920bcf333388d9e1561f0d6ed562852397e
Lis <lp598@kent.ac.uk>
2026-03-11 18:42:49 +0000

    Added visual menu

--- main.py
+++ main.py
@@ -2,6 +2,17 @@ from models import Contact, ContactManager
 
 manager = ContactManager()
 
+while True:
+    print("---Menu---")
+    print("1) Add Contact")
+    print("2) View Contacts")
+    print("3) Search Contacts")
+    print("4) Exit)")
+    choice = input("Option: ")
+
+    if choice == "1":
+        
+
 contact1 = Contact("Lis Pireva", "1234567890", "lis@example.com")
 contact2 = Contact("dave", "1234567899", "dave@example.com")
 
