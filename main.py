3007d38cd52fe1e5b087a4ad43ebaa22118a7637
Lis <lp598@kent.ac.uk>
2026-03-11 13:15:03 +0000

    adding more imports to main

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from models import Contact
+from models import Contact, ContactManager
 
-try:
-    contact1 = Contact("Lis Pireva", "1234567890", "lis@example.com")
-    print(contact1)
-except ValueError as error:
-    print(error)
+manager = ContactManager()
+
+contact1 = Contact("Lis Pireva", "1234567890", "lis@example.com")
+contact2 = Contact("dave", "1234567899", "dave@example.com")
+
+manager.add_contact(contact1)
+manager.add_contact(contact2)
+
+for contact in manager.get_all_contacts():
+    print(contact)
