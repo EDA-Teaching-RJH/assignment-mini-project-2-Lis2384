496ba12ed9c55cb5efec2cd523590a80558a546b
Lis <lp598@kent.ac.uk>
2026-03-11 13:21:48 +0000

    testing contacts

--- test_contacts.py
+++ test_contacts.py
@@ -1,5 +1,5 @@
 import unittest
-from models import Contact
+from models import Contact, ContactManager
 
 class TestContact(unittest.TestCase):
     def test_contact_creation(self):
@@ -13,6 +13,12 @@ class TestContact(unittest.TestCase):
     def test_invalid_phone(self):
         with self.assertRaises(ValueError):
                 Contact("Lis pireva", "435", "lis@example.com")
+    
+    def test_add_contact(self):
+        manager = ContactManager()
+        contact = Contact("Lis Pireva", "+447718833017", "lis@example.com")
+        manager.add_contact(contact)
+        self.assertEqual(len(manager.get_all_contacts()), 1)
 
 if __name__ == "__main__":
     unittest.main()
