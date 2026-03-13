6138341655e63c388e0352bb3299fd958eec5f75
Lis <lp598@kent.ac.uk>
2026-03-13 11:42:16 +0000

    comments added to main

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from models import Contact, ContactManager
+from models import Contact, ContactManager #Imports the contact and contactmanager from models
 
-manager = ContactManager("contacts.txt")
-manager.load_contacts()
+manager = ContactManager("contacts.txt") #connecting contact manager to contacts.txt
+manager.load_contacts() # Loads all saved contacts from the file
 
-while True:
+while True: #keeps showing menu untill you exit
     print("---Menu---")
     print("1) Add Contact")
     print("2) View Contacts")
@@ -12,11 +12,11 @@ while True:
     print("5) Exit")
     choice = input("Option: ")
 
-    if choice == "1":
+    if choice == "1": # Selects the first choice of adding a contact
         name = input("Name: ")
         phone = input("Phone Number: ")
         email = input("Email: ")
-        try:
+        try: #tries to create and save contact, also catches invalid inputs
             contact = Contact(name, phone, email)
             manager.add_contact(contact)
             manager.save_contacts()
@@ -25,32 +25,32 @@ while True:
             print(error)
         
     
-    elif choice == "2":
+    elif choice == "2": # Selects the second choice of showing all contacts saved
         for contact in manager.get_all_contacts():
             print(contact)
     
-    elif choice == "3":
+    elif choice == "3": # Selects the third option of searching a contact by email input
         email = input("Email you want to search: ")
         contact = manager.find_by_email(email)
-        if contact:
+        if contact: # Checks whether email is in system
             print(contact)
         else:
             print("Contact not found")
     
-    elif choice == "4":
+    elif choice == "4": # Selects the fourth choice of updating an existing contact
         email = input("current email: ")
         new_name = input("New name: ").strip()
         new_phone = input("New phone number: ").strip()
         new_email = input("New email: ").strip()
 
-        try:
+        try: # Tries to update contact details and check for invalid input
             updated = manager.update_contact(
                 email,
                 new_name if new_name else None,
                 new_phone if new_phone else None,
                 new_email if new_email else None
             )
-            manager.save_contacts()
+            manager.save_contacts() # Saves the contacts changed
             if updated:
                 print("Contact updated")
             else:
@@ -58,10 +58,10 @@ while True:
         except ValueError as error:
             print(error)
     
-    elif choice == "5":
+    elif choice == "5": # Selects option 5 which saves all contacts and ends the program
         manager.save_contacts()
         print("Goodbye")
         break
 
 for contact in manager.get_all_contacts():
-    print(contact)
+    print(contact) # Prints all contacts after the program ends (mostly just a safety check)
