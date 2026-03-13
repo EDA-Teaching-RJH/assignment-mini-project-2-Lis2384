455f0b572d257f2562c1bc062d42f5b298a947a8
Lis <lp598@kent.ac.uk>
2026-03-13 20:23:34 +0000

    comments added to test contacts

--- test_contacts.py
+++ test_contacts.py
@@ -1,35 +1,35 @@
-import unittest
-from models import Contact, ContactManager
-import os
-import tempfile
+import unittest #imports unittest module to create and run test cases
+from models import Contact, ContactManager #imports contact and contactmanager classes
+import os #imports os so the temp test file can be deleted after the test
+import tempfile # imports tempfile for save and load testing
 
-class TestContact(unittest.TestCase):
-    def test_contact_creation(self):
+class TestContact(unittest.TestCase): # makes test class for checking contact and contactmanager
+    def test_contact_creation(self): #test if contact is made correctly
         contact = Contact("Lis Pireva", "+447718833017", "lis@example.com")
         self.assertEqual(contact.get_email(), "lis@example.com")
 
-    def test_invalid_email(self):
+    def test_invalid_email(self): #test if a invalid email raises an error
         with self.assertRaises(ValueError):
             Contact("Lis Pireva", "+447718833017", "fsdfkjsvb")
 
-    def test_invalid_phone(self):
+    def test_invalid_phone(self): # test if a invalid phone raises error
         with self.assertRaises(ValueError):
                 Contact("Lis pireva", "435", "lis@example.com")
     
-    def test_add_contact(self):
+    def test_add_contact(self): # test if contact can be added to manager
         manager = ContactManager()
         contact = Contact("Lis Pireva", "+447718833017", "lis@example.com")
         manager.add_contact(contact)
         self.assertEqual(len(manager.get_all_contacts()), 1)
 
-    def test_find_by_email(self):
+    def test_find_by_email(self): #test if contact can be found by email
         manager = ContactManager()
         contact = Contact("Lis Pireva", "07718833017", "lis@example.com")
         manager.add_contact(contact)
         result = manager.find_by_email("lis@example.com")
         self.assertIsNotNone(result)
     
-    def test_update_contact(self):
+    def test_update_contact(self): #test if contact can be updated
         manager = ContactManager()
         contact = Contact("Lis Pireva", "07718833017", "lis@example.com")
         manager.add_contact(contact)
@@ -37,7 +37,7 @@ class TestContact(unittest.TestCase):
         result = manager.find_by_email("lis@example.com")
         self.assertEqual(result.get_name(), "Lis Updated")
 
-    def test_save_and_load(self):
+    def test_save_and_load(self): #test if contact can be saved to and loaded from
         temp = tempfile.NamedTemporaryFile(delete=False)
         temp.close()
 
@@ -52,5 +52,5 @@ class TestContact(unittest.TestCase):
         self.assertEqual(len(new_manager.get_all_contacts()), 1)
         os.remove(temp.name)
         
-if __name__ == "__main__":
+if __name__ == "__main__":  #run the unit tests when file is executed
     unittest.main()
